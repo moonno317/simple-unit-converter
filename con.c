5a795d55c0a9f08f44e50fa70af085e97d5ccbb9
moonno317 <139625145+moonno317@users.noreply.github.com>
2023-09-02 11:21:57 +0700

    Add files via upload

---
+++ con.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	float meter, centimeter, mps, kph;
+	int number;
+	
+	printf("1: Meter to Centimeter\n2: Meter per second to Kilometer per hour\n> ");
+	scanf("%d", &number);
+	
+	if (number == 1) {
+		printf("Enter the centimeter\n> ");
+		scanf("%f", &meter);
+		meter = meter * 100;
+		printf("%f", meter); }
+       	else if(number == 2) {
+		printf("Enter the mps\n> ");
+		scanf("%f", &mps);
+		mps = mps * 3.6;
+		printf("%f", mps); }
+	return 0; }
